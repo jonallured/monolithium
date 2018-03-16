57814a775f5706685853f0144289cb10e09e04de
Jon Allured <jon.allured@gmail.com>
2018-03-16 13:20:06 -0500

    Clean up config

--- config/application.rb
+++ config/application.rb
@@ -18,14 +18,7 @@ Bundler.require(*Rails.groups)
 
 module Monolithium
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
     config.generators.system_tests = nil
+    config.load_defaults 5.1
   end
 end
