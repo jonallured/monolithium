f7725a4396ab24d407b26d14932aa900b9d014cf
Jon Allured <jon.allured@gmail.com>
2018-03-25 21:31:02 -0500

    Add simple admin password authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :ensure_admin
+
+  private
+
+  def session_password_matches?
+    Rails.application.secrets[:admin_password] == session[:admin_password]
+  end
+
+  def ensure_admin
+    return if session_password_matches?
+    session.clear
+    redirect_to sign_in_path
+  end
 end
