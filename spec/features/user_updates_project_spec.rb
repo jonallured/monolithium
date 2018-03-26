fa8436bad0f8bec38b81664289f74732b3c7127d
Jon Allured <jon.allured@gmail.com>
2018-03-26 09:05:57 -0500

    Extract duplication and improve coverage
    Here I've extracted helpers to mock out the authentication stuff and
    also to ensure you're redirected when not logged in.

--- spec/features/user_updates_project_spec.rb
+++ spec/features/user_updates_project_spec.rb
@@ -1,21 +1,20 @@
 require 'rails_helper'
 
 feature 'User updates project', js: true do
-  before do
-    allow_any_instance_of(ApplicationController)
-      .to receive(:session_password_matches?).and_return(true)
-  end
+  context 'signed in as admin' do
+    include_context 'session password matches'
 
-  scenario 'project is updated' do
-    FactoryBot.create :project, name: 'Older'
-    project = FactoryBot.create :project
-    FactoryBot.create :project, name: 'Newer'
+    scenario 'project is updated' do
+      FactoryBot.create :project, name: 'Older'
+      project = FactoryBot.create :project
+      FactoryBot.create :project, name: 'Newer'
 
-    visit '/projects'
-    project_item = page.find('li', text: project.name)
-    project_item.click
+      visit '/projects'
+      project_item = page.find('li', text: project.name)
+      project_item.click
 
-    touched_projects = page.all('.touched')
-    expect(touched_projects.count).to eq 1
+      touched_projects = page.all('.touched')
+      expect(touched_projects.count).to eq 1
+    end
   end
 end
--- spec/features/user_views_project_list_spec.rb
+++ spec/features/user_views_project_list_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 feature 'User views project list', js: true do
-  before do
-    allow_any_instance_of(ApplicationController)
-      .to receive(:session_password_matches?).and_return(true)
-  end
+  include_examples 'admin password required', '/projects'
 
-  scenario 'with no projects' do
-    visit '/projects'
-    expect(page).to have_content 'No projects - create one!'
-  end
+  context 'signed in as admin' do
+    include_context 'session password matches'
 
-  scenario 'with a few projects' do
-    FactoryBot.create :project, name: '1st', touched_at: Time.zone.now + 1.day
-    FactoryBot.create :project, name: '2nd', touched_at: Time.zone.now - 1.day
-    FactoryBot.create :project, name: '3rd', touched_at: Time.zone.now
+    scenario 'with no projects' do
+      visit '/projects'
+      expect(page).to have_content 'No projects - create one!'
+    end
 
-    visit '/projects'
+    scenario 'with a few projects' do
+      FactoryBot.create :project, name: '1st', touched_at: Time.zone.now + 1.day
+      FactoryBot.create :project, name: '2nd', touched_at: Time.zone.now - 1.day
+      FactoryBot.create :project, name: '3rd', touched_at: Time.zone.now
 
-    project_names = page.all('li .name').map(&:text)
-    expect(project_names).to eq %w[2nd 3rd 1st]
+      visit '/projects'
+
+      project_names = page.all('li .name').map(&:text)
+      expect(project_names).to eq %w[2nd 3rd 1st]
+    end
   end
 end
