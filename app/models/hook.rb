4429c617ce3687a704c89eec90d00dd9ab7285e8
Jon Allured <jon.allured@gmail.com>
2018-05-22 15:37:26 -0500

    Ensure we're only showing Artsy opened PRs

--- app/models/hook.rb
+++ app/models/hook.rb
@@ -2,7 +2,20 @@ class Hook < ApplicationRecord
   validates :payload, presence: true
 
   def artsy_pull_request?
-    login = payload.dig('repository', 'owner', 'login')
-    login == 'artsy'
+    owner_login == 'artsy' && action == 'opened' && pull_request?
+  end
+
+  private
+
+  def owner_login
+    payload.dig('repository', 'owner', 'login')
+  end
+
+  def action
+    payload['action']
+  end
+
+  def pull_request?
+    payload.keys.include?('pull_request')
   end
 end
