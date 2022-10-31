6048e8fe030ca2c7db22e09a3115b4b7c5948424
Jon Allured <jon.allured@gmail.com>
2022-10-30 21:40:22 -0500

    Create basic Artsy Viewer page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,111 @@
 //
 //= require rails-ujs
 //= require_tree .
+
+const tenMinutesInMS = 600000
+const params = new URLSearchParams(window.location.search)
+
+const ArtsyViewer = {
+  artworks: [],
+  currentIndex: 0,
+  feedUrls: [
+    "https://artsy-public.s3.amazonaws.com/artworks-of-the-day/2022-10-26.json",
+    "https://artsy-public.s3.amazonaws.com/artworks-of-the-day/2022-10-27.json",
+    "https://artsy-public.s3.amazonaws.com/artworks-of-the-day/2022-10-28.json",
+    "https://artsy-public.s3.amazonaws.com/artworks-of-the-day/2022-10-29.json",
+  ],
+  tags: {},
+  viewTime: params.get("viewTime") || tenMinutesInMS,
+}
+
+const getNextArtwork = () => {
+  if (ArtsyViewer.currentIndex === ArtsyViewer.artworks.length) {
+    ArtsyViewer.currentIndex = 0
+  }
+
+  const nextArtwork = ArtsyViewer.artworks[ArtsyViewer.currentIndex]
+  ArtsyViewer.currentIndex += 1
+
+  return nextArtwork
+}
+
+const updateSection = (section) => {
+  const artwork = getNextArtwork()
+
+  const image = artwork.images[0]
+  const imageUrl = image.image_urls.normalized
+  const widthValue = ArtsyViewer.heightValue * image.aspect_ratio
+  const widthProperty = `${widthValue}px`
+
+  section.imageTag.src = imageUrl
+  section.imageTag.style.width = widthProperty
+
+  section.sectionTag.style.width = widthProperty
+  section.footerTag.style.width = widthProperty
+  section.parTag.textContent = artwork.display
+}
+
+const hideFirstFooter = () => {
+  ArtsyViewer.tags.firstSection.footerTag.classList.add('hide')
+  ArtsyViewer.tags.lastSection.footerTag.classList.remove('hide')
+
+  updateSection(ArtsyViewer.tags.lastSection)
+}
+
+const hideSecondFooter = () => {
+  ArtsyViewer.tags.lastSection.footerTag.classList.add('hide')
+  ArtsyViewer.tags.firstSection.footerTag.classList.remove('hide')
+
+  updateSection(ArtsyViewer.tags.firstSection)
+}
+
+const sendFirstToBack = () => {
+  ArtsyViewer.tags.firstSection.sectionTag.classList.add("back")
+  ArtsyViewer.tags.lastSection.sectionTag.classList.remove("back")
+
+  setTimeout(hideSecondFooter, ArtsyViewer.viewTime / 2)
+  setTimeout(sendLastToBack, ArtsyViewer.viewTime)
+}
+
+const sendLastToBack = () => {
+  ArtsyViewer.tags.firstSection.sectionTag.classList.remove("back")
+  ArtsyViewer.tags.lastSection.sectionTag.classList.add("back")
+
+  setTimeout(hideFirstFooter, ArtsyViewer.viewTime / 2)
+  setTimeout(sendFirstToBack, ArtsyViewer.viewTime)
+}
+
+const getSection = (sectionTag) => {
+  return {
+    footerTag: sectionTag.querySelector('footer'),
+    imageTag: sectionTag.querySelector('img'),
+    parTag: sectionTag.querySelector('p'),
+    sectionTag: sectionTag,
+  }
+}
+
+const setupArtsyViewer = () => {
+  ArtsyViewer.tags.main = document.querySelector('main')
+  const [firstSection, lastSection] = document.getElementsByTagName('section')
+  ArtsyViewer.tags.firstSection = getSection(firstSection)
+  ArtsyViewer.tags.lastSection = getSection(lastSection)
+  const mainStyles = window.getComputedStyle(ArtsyViewer.tags.main)
+  const heightProperty = mainStyles.getPropertyValue('height')
+  const heightValue = heightProperty.split("px")[0] - 1
+  ArtsyViewer.heightValue = heightValue
+
+  hideSecondFooter()
+  sendLastToBack()
+}
+
+const getFeeds = () => {
+  const promises = ArtsyViewer.feedUrls.map(feedUrl => {
+    return fetch(feedUrl)
+      .then((response) => response.json())
+      .then((data) => ArtsyViewer.artworks = ArtsyViewer.artworks.concat(data))
+  })
+
+  Promise.all(promises).then(setupArtsyViewer)
+}
+
+window.onload = getFeeds
---
+++ app/controllers/artsy_viewer_controller.rb
@@ -0,0 +1,2 @@
+class ArtsyViewerController < ApplicationController
+end
