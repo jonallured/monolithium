e781198cafd6d14c88e223e0d14ce563c8b0b375
Jon Allured <jon.allured@gmail.com>
2022-11-06 14:17:21 -0600

    Add MP client

---
+++ app/clients/metaphysics.rb
@@ -0,0 +1,49 @@
+require 'graphql/client'
+require 'graphql/client/http'
+
+class Metaphysics
+  ENDPOINT_URL = 'https://metaphysics-production.artsy.net/v2'.freeze
+
+  def self.fetch_auth_token
+    commands = <<-BASH
+      cd ../metaphysics
+      hokusai production env get INTROSPECT_TOKEN \
+        | cut -f 2 -d "=" \
+        > ../monolithium/introspection_token.txt
+      cd ../monolithium
+    BASH
+
+    system commands
+  end
+
+  def self.generate_raw_client
+    fetch_auth_token unless File.exist?('introspection_token.txt')
+
+    GraphQL::Client::HTTP.new(ENDPOINT_URL) do
+      def headers(_context) # rubocop:disable Lint/NestedMethodDefinition
+        introspection_token = File.read('introspection_token.txt').chomp
+        { 'Authorization' => "Bearer #{introspection_token}" }
+      end
+    end
+  end
+
+  def self.raw_http_client
+    @raw_http_client ||= generate_raw_client
+  end
+
+  def self.generate_schema
+    local_schema_path = 'mp_schema.json'
+
+    GraphQL::Client.dump_schema(raw_http_client, local_schema_path) unless File.exist?(local_schema_path)
+
+    GraphQL::Client.load_schema(local_schema_path)
+  end
+
+  def self.schema
+    @schema ||= generate_schema
+  end
+
+  def self.client
+    @client ||= GraphQL::Client.new(schema: schema, execute: raw_http_client)
+  end
+end
