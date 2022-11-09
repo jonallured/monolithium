e0b7bfd413a6e7bb07bbc4619ac883b0e7e8ca77
Jon Allured <jon.allured@gmail.com>
2022-11-09 12:46:46 -0600

    Use lineups as of time now to power viewer

--- app/channels/artsy_viewer_channel.rb
+++ app/channels/artsy_viewer_channel.rb
@@ -1,13 +1,21 @@
 class ArtsyViewerChannel < ApplicationCable::Channel
   CHANNEL_ID = 'artsy_viewer'.freeze
 
+  def self.broadcast(payload)
+    ActionCable.server.broadcast(
+      CHANNEL_ID,
+      payload
+    )
+  end
+
   def subscribed
     transmit initial_payload
+    stream_from CHANNEL_ID
   end
 
   private
 
   def initial_payload
-    Lineup.current.artworks
+    Lineup.current.as_of(Time.zone.now)
   end
 end
