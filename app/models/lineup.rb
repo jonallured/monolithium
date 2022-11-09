f3e76ff8d0260d5e653fc5b6599dabf2e744203c
Jon Allured <jon.allured@gmail.com>
2022-11-08 21:42:41 -0600

    Power viewer with current lineup

--- app/models/lineup.rb
+++ app/models/lineup.rb
@@ -1,3 +1,8 @@
 class Lineup < ApplicationRecord
   has_many :lineup_artworks, dependent: :destroy
+  has_many :artworks, through: :lineup_artworks
+
+  def self.current
+    find_by(current_on: Time.zone.today)
+  end
 end
