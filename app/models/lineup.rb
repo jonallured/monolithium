dd65bc941cf90ce29c5cf948035ee111a298335f
Jon Allured <jon.allured@gmail.com>
2022-11-08 21:55:15 -0600

    Create a new lineup every day

--- app/models/lineup.rb
+++ app/models/lineup.rb
@@ -2,7 +2,22 @@ class Lineup < ApplicationRecord
   has_many :lineup_artworks, dependent: :destroy
   has_many :artworks, through: :lineup_artworks
 
+  after_create :choose_artworks
+
+  def self.create_next
+    create(current_on: current.current_on + 1.day)
+  end
+
   def self.current
     find_by(current_on: Time.zone.today)
+  end
+
+  private
+
+  def choose_artworks
+    artwork_ids = Artwork.pluck(:id).sample(24)
+    artwork_ids.each_with_index do |artwork_id, index|
+      lineup_artworks.create(artwork_id: artwork_id, position: index + 1)
+    end
   end
 end
