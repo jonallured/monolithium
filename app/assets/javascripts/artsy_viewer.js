ec0b91a3021df7e3db47038241a61673cfddaf6e
Jon Allured <jon.allured@gmail.com>
2022-11-11 15:51:44 -0600

    Use a single codepath for all artwork rendering

--- app/assets/javascripts/artsy_viewer.js
+++ app/assets/javascripts/artsy_viewer.js
@@ -14,79 +14,21 @@ window.onload = () => {
 
   App.ArtsyViewer = ArtsyViewer
 
-  App.cable.subscriptions.create('ArtsyViewerChannel', {
-    received: (data) => {
-      if (App.ArtsyViewer.artworks.length === data.length) return
+  const drawSection = (artwork, index) => {
+    const { blurb, href, image } = artwork.payload
 
-      App.ArtsyViewer.artworks = data
+    const blurbNode = document.createTextNode(blurb)
+    const parElement = document.createElement("p")
+    parElement.appendChild(blurbNode)
 
-      ArtsyViewer.elements.main.replaceChildren()
+    const footerElement = document.createElement("footer")
+    footerElement.appendChild(parElement)
 
-      const [first, ...rest] = ArtsyViewer.artworks
+    const imageElement = document.createElement("img")
 
-      rest.forEach(artwork => {
-        const { blurb, href, image } = artwork.payload
-
-        const blurbNode = document.createTextNode(blurb)
-        const parElement = document.createElement("p")
-        parElement.appendChild(blurbNode)
-
-        const footerElement = document.createElement("footer")
-        footerElement.appendChild(parElement)
-
-        const imageElement = document.createElement("img")
-        const widthValue = (ArtsyViewer.heightValue - 70) * image.aspect_ratio
-        const widthProperty = `${widthValue}px`
-
-        imageElement.src = image.url
-        imageElement.style.width = widthProperty
-
-        const aElement = document.createElement("a")
-        const url = `https://www.artsy.net${href}`
-
-        aElement.href = url
-
-        aElement.appendChild(imageElement)
-        aElement.appendChild(footerElement)
-
-        const sectionElement = document.createElement("section")
-        sectionElement.appendChild(aElement)
-
-        ArtsyViewer.elements.main.appendChild(sectionElement)
-      })
-
-      const { blurb, href, image } = first.payload
-
-      const blurbNode = document.createTextNode(blurb)
-      const parElement = document.createElement("p")
-      parElement.appendChild(blurbNode)
-
-      const footerElement = document.createElement("footer")
-      footerElement.appendChild(parElement)
-
-      const imageElement = document.createElement("img")
-      const imageHeight = ArtsyViewer.heightValue - 70
-      const widthValue = imageHeight * image.aspect_ratio
-      const widthProperty = `${widthValue}px`
-
-      imageElement.src = image.url
-      imageElement.style.height = `${imageHeight}px`
-      imageElement.style.width = widthProperty
+    if (index === 0) {
       imageElement.classList.add("collapsed")
       imageElement.classList.add("hidden")
-
-      const aElement = document.createElement("a")
-      const url = `https://www.artsy.net${href}`
-
-      aElement.href = url
-
-      aElement.appendChild(imageElement)
-      aElement.appendChild(footerElement)
-
-      const sectionElement = document.createElement("section")
-      sectionElement.appendChild(aElement)
-
-      ArtsyViewer.elements.main.prepend(sectionElement)
 
       setTimeout(() => {
         imageElement.classList.remove("collapsed")
@@ -94,6 +36,36 @@ window.onload = () => {
           imageElement.classList.remove("hidden")
         }, 500)
       }, 500)
+    }
+
+    const imageHeight = ArtsyViewer.heightValue - 70
+    const widthValue = imageHeight * image.aspect_ratio
+    const widthProperty = `${widthValue}px`
+
+    imageElement.src = image.url
+    imageElement.style.width = widthProperty
+
+    const aElement = document.createElement("a")
+    const url = `https://www.artsy.net${href}`
+
+    aElement.href = url
+
+    aElement.appendChild(imageElement)
+    aElement.appendChild(footerElement)
+
+    const sectionElement = document.createElement("section")
+    sectionElement.appendChild(aElement)
+
+    ArtsyViewer.elements.main.appendChild(sectionElement)
+  }
+
+  App.cable.subscriptions.create('ArtsyViewerChannel', {
+    received: (data) => {
+      if (ArtsyViewer.artworks.length === data.length) return
+
+      ArtsyViewer.elements.main.replaceChildren()
+      ArtsyViewer.artworks = data
+      ArtsyViewer.artworks.forEach(drawSection)
     }
   })
 }
