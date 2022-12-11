a9bef186cc3629b963eb922873b810acdadea35b
Jon Allured <jon.allured@gmail.com>
2022-12-11 14:41:52 -0600

    Add client for books api

---
+++ app/clients/open_library.rb
@@ -0,0 +1,24 @@
+class OpenLibrary
+  ENDPOINT_URL = 'https://openlibrary.org'.freeze
+
+  def self.generate_client
+    Faraday.new(
+      url: ENDPOINT_URL,
+      headers: { 'Content-Type' => 'application/json' }
+    ) do |f|
+      f.adapter Faraday.default_adapter
+      f.response :follow_redirects
+      f.response :json
+    end
+  end
+
+  def self.client
+    @client ||= generate_client
+  end
+
+  def self.get_book(isbn)
+    url = "/isbn/#{isbn}.json"
+    response = client.get(url)
+    response.body
+  end
+end
