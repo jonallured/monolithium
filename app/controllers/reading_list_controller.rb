149497463e4b24d6cbef6ad894beb18c3b6dd70b
Jon Allured <jon.allured@gmail.com>
2023-03-04 11:21:01 -0700

    Only show books from specified year

--- app/controllers/reading_list_controller.rb
+++ app/controllers/reading_list_controller.rb
@@ -1,7 +1,17 @@
 class ReadingListController < ApplicationController
   expose(:year) { params[:year] }
-  expose(:books) { Book.all.order(finished_on: :asc, pages: :desc) }
+
+  expose(:books) do
+    start_finished = DateTime.parse("#{year}-01-01")
+    stop_finished = start_finished.end_of_year
+
+    Book
+      .where(finished_on: start_finished..stop_finished)
+      .order(finished_on: :asc, pages: :desc)
+  end
+
   expose(:total_pages) { books.pluck(:pages).compact.sum }
+
   expose(:pace) do
     days_so_far = Time.zone.today.mjd - Time.zone.today.beginning_of_year.mjd
     exact_pace = (total_pages * 1.0) / days_so_far
