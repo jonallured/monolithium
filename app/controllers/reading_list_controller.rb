5dfbf52adcb0a7b74626fc14e017846a99cb9775
Jon Allured <jon.allured@gmail.com>
2023-03-08 11:23:38 -0700

    Reorganize system specs

--- app/controllers/reading_list_controller.rb
+++ app/controllers/reading_list_controller.rb
@@ -1,3 +1,4 @@
 class ReadingListController < ApplicationController
+  skip_before_action :ensure_admin
   expose(:reading_list) { ReadingList.new(params[:year].to_i) }
 end
--- spec/support/response_json.rb
+++
@@ -1,9 +0,0 @@
-module ResponseJSON
-  def response_json
-    JSON.parse(response.body)
-  end
-end
-
-RSpec.configure do |config|
-  config.include ResponseJSON
-end
