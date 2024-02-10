4875ea500ac0a3bf9caa0b12ec76d3ea5e6ec9b2
Jon Allured <jon@jonallured.com>
2024-02-10 16:19:51 -0600

    Admin views financial report

--- app/models/financial_statement.rb
+++ app/models/financial_statement.rb
@@ -5,4 +5,8 @@ class FinancialStatement < ApplicationRecord
   validates_uniqueness_of :period_start_on, scope: :financial_account
 
   scope :for_year, ->(year) { where("extract(year from period_start_on) = ?", year) }
+
+  def net_amount_cents
+    ending_amount_cents - starting_amount_cents
+  end
 end
---
+++ app/models/null_statement.rb
@@ -0,0 +1,12 @@
+class NullStatement
+  include Singleton
+
+  def starting_amount_cents
+  end
+
+  def ending_amount_cents
+  end
+
+  def net_amount_cents
+  end
+end
