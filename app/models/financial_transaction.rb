89c4e299f1804d6b4e0fdbd83041b96205d43882
Jon Allured <jon@jonallured.com>
2024-02-12 20:03:12 -0600

    Include income and expenses in report

--- app/models/financial_transaction.rb
+++ app/models/financial_transaction.rb
@@ -2,4 +2,6 @@ class FinancialTransaction < ApplicationRecord
   belongs_to :financial_account
 
   validates_presence_of :posted_on, :amount_cents, :description
+
+  scope :for_year, ->(year) { where("extract(year from posted_on) = ?", year) }
 end
--- spec/system/financial_reports/admin_views_financial_report_spec.rb
+++ spec/system/financial_reports/admin_views_financial_report_spec.rb
@@ -14,8 +14,8 @@ describe "Admin views financial reports" do
   scenario "with no statements for that year" do
     visit "/financial_reports/#{last_year}"
 
-    header = page.find("header nav h1")
-    expect(header.text).to eq last_year.year.to_s
+    header_h1 = page.find("header nav h1")
+    expect(header_h1.text).to eq last_year.year.to_s
 
     prev_link, next_link = page.all("header nav a").to_a
 
@@ -25,22 +25,59 @@ describe "Admin views financial reports" do
     expect(next_link.text).to eq "next"
     expect(next_link["href"]).to end_with((last_year.year + 1).to_s)
 
-    page.all("table").to_a.each do |table|
-      month_row, starting_row, ending_row, net_row = table.all("tr").to_a
+    checking_table, savings_table = page.all("table").to_a
 
-      expect(month_row.all("th").map(&:text)).to eq %w[
+    within checking_table do
+      checking_trs = checking_table.all("tr").to_a
+
+      month_tr = checking_trs[0]
+      starting_tr = checking_trs[1]
+      income_tr = checking_trs[2]
+      expenses_tr = checking_trs[3]
+      ending_tr = checking_trs[4]
+      net_tr = checking_trs[5]
+
+      expect(month_tr.all("th").map(&:text)).to eq %w[
         Month Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
       ]
 
-      expect(starting_row.all("td").map(&:text)).to eq %w[
+      expect(starting_tr.all("td").map(&:text)).to eq %w[
         Starting - - - - - - - - - - - -
       ]
 
-      expect(ending_row.all("td").map(&:text)).to eq %w[
+      expect(income_tr.all("td").map(&:text)).to eq %w[
+        Income - - - - - - - - - - - -
+      ]
+
+      expect(expenses_tr.all("td").map(&:text)).to eq %w[
+        Expenses - - - - - - - - - - - -
+      ]
+
+      expect(ending_tr.all("td").map(&:text)).to eq %w[
         Ending - - - - - - - - - - - -
       ]
 
-      expect(net_row.all("td").map(&:text)).to eq %w[
+      expect(net_tr.all("td").map(&:text)).to eq %w[
+        Net - - - - - - - - - - - -
+      ]
+    end
+
+    within savings_table do
+      month_tr, starting_tr, ending_tr, net_tr = savings_table.all("tr").to_a
+
+      expect(month_tr.all("th").map(&:text)).to eq %w[
+        Month Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
+      ]
+
+      expect(starting_tr.all("td").map(&:text)).to eq %w[
+        Starting - - - - - - - - - - - -
+      ]
+
+      expect(ending_tr.all("td").map(&:text)).to eq %w[
+        Ending - - - - - - - - - - - -
+      ]
+
+      expect(net_tr.all("td").map(&:text)).to eq %w[
         Net - - - - - - - - - - - -
       ]
     end
@@ -58,10 +95,22 @@ describe "Admin views financial reports" do
     visit "/financial_reports/#{last_year}"
 
     checking_table = page.all("table").to_a.first
-    _month_tr, starting_tr, ending_tr, net_tr = checking_table.all("tr").to_a
+    checking_trs = checking_table.all("tr").to_a
+
+    starting_tr = checking_trs[1]
+    income_tr = checking_trs[2]
+    expenses_tr = checking_trs[3]
+    ending_tr = checking_trs[4]
+    net_tr = checking_trs[5]
 
     may_starting_td = starting_tr.all("td").to_a[5]
     expect(may_starting_td.text).to eq "$1,034.89"
+
+    may_income_td = income_tr.all("td").to_a[5]
+    expect(may_income_td.text).to eq "-"
+
+    may_expenses_td = expenses_tr.all("td").to_a[5]
+    expect(may_expenses_td.text).to eq "-"
 
     may_ending_td = ending_tr.all("td").to_a[5]
     expect(may_ending_td.text).to eq "$99.00"
@@ -94,7 +143,11 @@ describe "Admin views financial reports" do
     checking_table, savings_table = page.all("table").to_a
 
     within checking_table do
-      _month_tr, starting_tr, ending_tr, net_tr = checking_table.all("tr").to_a
+      checking_trs = checking_table.all("tr").to_a
+
+      starting_tr = checking_trs[1]
+      ending_tr = checking_trs[4]
+      net_tr = checking_trs[5]
 
       _starting_label, *starting_tds = starting_tr.all("td").to_a
       expect(starting_tds.count).to eq 12
@@ -146,7 +199,11 @@ describe "Admin views financial reports" do
     visit "/financial_reports/#{last_year}"
 
     checking_table = page.all("table").to_a.first
-    _month_tr, starting_tr, ending_tr, net_tr = checking_table.all("tr").to_a
+    checking_trs = checking_table.all("tr").to_a
+
+    starting_tr = checking_trs[1]
+    ending_tr = checking_trs[4]
+    net_tr = checking_trs[5]
 
     may_starting_td = starting_tr.all("td").to_a[5]
     expect(may_starting_td.text).to eq "$170.00"
@@ -156,5 +213,55 @@ describe "Admin views financial reports" do
 
     may_net_td = net_tr.all("td").to_a[5]
     expect(may_net_td.text).to eq "($100.00)"
+  end
+
+  scenario "with one statement in the middle of that year" do
+    FactoryBot.create(
+      :financial_statement,
+      financial_account: FinancialAccount.wf_checking,
+      period_start_on: Date.parse("#{last_year.year}-05-01"),
+      starting_amount_cents: 1_034_89,
+      ending_amount_cents: 99_00
+    )
+
+    FactoryBot.create(
+      :financial_transaction,
+      financial_account: FinancialAccount.wf_checking,
+      posted_on: Date.parse("#{last_year.year}-05-01"),
+      amount_cents: 900_00
+    )
+
+    FactoryBot.create(
+      :financial_transaction,
+      financial_account: FinancialAccount.wf_checking,
+      posted_on: Date.parse("#{last_year.year}-05-01"),
+      amount_cents: -1_835_89
+    )
+
+    visit "/financial_reports/#{last_year}"
+
+    checking_table = page.all("table").to_a.first
+    checking_trs = checking_table.all("tr").to_a
+
+    starting_tr = checking_trs[1]
+    income_tr = checking_trs[2]
+    expenses_tr = checking_trs[3]
+    ending_tr = checking_trs[4]
+    net_tr = checking_trs[5]
+
+    may_starting_td = starting_tr.all("td").to_a[5]
+    expect(may_starting_td.text).to eq "$1,034.89"
+
+    may_income_td = income_tr.all("td").to_a[5]
+    expect(may_income_td.text).to eq "$900.00"
+
+    may_expenses_td = expenses_tr.all("td").to_a[5]
+    expect(may_expenses_td.text).to eq "($1,835.89)"
+
+    may_ending_td = ending_tr.all("td").to_a[5]
+    expect(may_ending_td.text).to eq "$99.00"
+
+    may_net_td = net_tr.all("td").to_a[5]
+    expect(may_net_td.text).to eq "($935.89)"
   end
 end
