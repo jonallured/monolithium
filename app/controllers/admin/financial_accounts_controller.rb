3bd57bd516d41fbe429b474ff5f4093859767ed2
Jon Allured <jon@jonallured.com>
2024-02-18 14:00:49 -0800

    Admin edits financial account

--- app/controllers/admin/financial_accounts_controller.rb
+++ app/controllers/admin/financial_accounts_controller.rb
@@ -14,6 +14,16 @@ class Admin::FinancialAccountsController < ApplicationController
     end
   end
 
+  def update
+    if financial_account.update(financial_account_params)
+      flash.notice = "Financial Account successfully updated"
+      redirect_to admin_financial_account_path(financial_account)
+    else
+      flash.alert = financial_account.errors.full_messages.join(",")
+      redirect_to edit_admin_financial_account_path(financial_account)
+    end
+  end
+
   private
 
   def financial_account_params
