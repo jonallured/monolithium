1e0a5e50908be3fec7a5062319e166ec202e4525
Jon Allured <jon@jonallured.com>
2024-02-18 20:52:47 -0800

    Add presence validation for gift ideas

--- spec/models/gift_idea_spec.rb
+++ spec/models/gift_idea_spec.rb
@@ -1,6 +1,26 @@
 require "rails_helper"
 
 describe GiftIdea do
+  describe "validation" do
+    context "without required attrs" do
+      it "is invalid" do
+        gift_idea = GiftIdea.new
+        expect(gift_idea).to_not be_valid
+      end
+    end
+
+    context "with required attrs" do
+      it "is valid" do
+        gift_idea = GiftIdea.new(
+          title: "Graphic Novel",
+          website_url: "https://www.marvel.com/graphic-novel.html"
+        )
+
+        expect(gift_idea).to be_valid
+      end
+    end
+  end
+
   describe "scopes" do
     before do
       FactoryBot.create_list :available_gift_idea, 3
