7d54a7c7a779b689ada3c77ceff01ec4a5735418
Jon Allured <jon@jonallured.com>
2024-02-18 20:52:47 -0800

    Add presence validation for gift ideas

--- app/models/gift_idea.rb
+++ app/models/gift_idea.rb
@@ -1,4 +1,6 @@
 class GiftIdea < ApplicationRecord
+  validates_presence_of :title, :website_url
+
   scope :available, -> { where("claimed_at IS NULL AND received_at IS NULL") }
   scope :claimed, -> { where("claimed_at IS NOT NULL AND received_at IS NULL") }
   scope :received, -> { where("received_at IS NOT NULL") }
