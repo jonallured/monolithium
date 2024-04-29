4d9d398137fb3c073adace4054a9dc35fdf82536
Jon Allured <jon@jonallured.com>
2024-04-29 14:52:35 -0500

    Add support for more financial account categories

--- app/models/financial_account.rb
+++ app/models/financial_account.rb
@@ -1,13 +1,19 @@
 class FinancialAccount < ApplicationRecord
+  CATEGORIES = %w[
+    checking
+    savings
+    health_savings
+    good_debt
+    bad_debt
+    retirement
+  ]
+
   has_many :financial_statements, dependent: :destroy
   has_many :financial_transactions, dependent: :destroy
 
   validates_presence_of :name
   validates_uniqueness_of :name
-  validates_inclusion_of :category, in: %w[checking savings]
-
-  scope :checking, -> { where(category: "checking") }
-  scope :savings, -> { where(category: "savings") }
+  validates_inclusion_of :category, in: CATEGORIES
 
   def table_attrs
     [
