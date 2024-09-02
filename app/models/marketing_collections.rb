5d6b943955ab3318ec0d91afdb6289fbc297f27f
Jon Allured <jon@jonallured.com>
2024-09-01 20:38:09 -0500

    Load marketing collections more safely

--- app/models/marketing_collections.rb
+++ app/models/marketing_collections.rb
@@ -5,7 +5,6 @@ class MarketingCollections
     contemporary-now
     finds-under-1000-dollars
     iconic-prints
-    street-art-highlights
     top-auction-lots
     trending-this-week
     trove-editors-picks
@@ -14,6 +13,8 @@ class MarketingCollections
   def self.load_artworks
     SLUGS.each do |slug|
       data = MetaphysicsApi.marketing_collection(slug)
+      next if data.marketing_collection.nil?
+
       nodes = data.marketing_collection.artworks_connection.edges.map(&:node)
       new_nodes = nodes.reject { |node| Artwork.exists?(gravity_id: node.gravity_id) }
       new_nodes.each do |node|
