6bdc747aca1bf7b51771db864612c35509c0eb79
Jon Allured <jon@jonallured.com>
2024-09-04 09:37:50 -0500

    Improve live post bin links

--- spec/system/post_bin/admin_views_post_bin_spec.rb
+++ spec/system/post_bin/admin_views_post_bin_spec.rb
@@ -4,18 +4,18 @@ describe "Admin views post bin", js: true do
   include_context "admin password matches"
 
   scenario "with an initial post bin request and then another is created" do
-    FactoryBot.create(:post_bin_request, body: "first one")
+    first_one = FactoryBot.create(:post_bin_request, body: "first one")
 
     visit "/post_bin"
 
-    expect(page).to have_css "li a", text: "first one"
+    expect(page).to have_css "li a", text: "#{first_one.id} first one"
     expect(page.all("li").count).to eq 1
 
     perform_enqueued_jobs do
-      FactoryBot.create(:post_bin_request, body: "second one")
-    end
+      second_one = FactoryBot.create(:post_bin_request, body: "")
 
-    expect(page).to have_css "li a", text: "second one"
-    expect(page.all("li").count).to eq 2
+      expect(page).to have_css "li a", text: second_one.id
+      expect(page.all("li").count).to eq 2
+    end
   end
 end
