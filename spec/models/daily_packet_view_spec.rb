117eb858bf7ab7351d2ff70a4a220452d8da53e4
Jon Allured <jon@jonallured.com>
2024-10-19 14:04:31 -0500

    Add very basic daily packet

---
+++ spec/models/daily_packet_view_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe DailyPacketView do
+  it "renders the document" do
+    built_on = Date.new(2007, 7, 7)
+    view = DailyPacketView.new(built_on)
+    pdf_data = view.document.render
+    inspector = PDF::Inspector::Text.analyze(pdf_data)
+    expect(inspector.strings).to eq([
+      "Daily Packet",
+      "07/07/2007, week 27",
+      "Random Warm Fuzzy",
+      "Reading Pace",
+      "0.0 pages/day"
+    ])
+  end
+end
