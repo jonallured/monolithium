9e087af883c2b1c1fc2c77a7554667b651929e70
Jon Allured <jon@jonallured.com>
2024-11-02 13:02:08 -0500

    Support producing daily packets locally

--- app/models/daily_packet.rb
+++ app/models/daily_packet.rb
@@ -1,4 +1,10 @@
 class DailyPacket
+  def self.save_locally(date = Date.today)
+    file_path = "tmp/daily_packet.pdf"
+    packet = DailyPacketView.new(date)
+    packet.save_as(file_path)
+  end
+
   def self.save_to_s3(date = Date.today)
     file_path = "daily-packets/#{date.strftime("%Y-%m-%d")}.pdf"
     packet = DailyPacketView.new(date)
