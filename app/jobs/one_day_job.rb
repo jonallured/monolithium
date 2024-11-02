f43cc922613b62acb9f8f4fc6a72ffef6b9f37a0
Jon Allured <jon@jonallured.com>
2024-11-02 12:52:32 -0500

    Use better name for saving daily packet to s3
    Also defaults to today but can accept any date.

--- app/jobs/one_day_job.rb
+++ app/jobs/one_day_job.rb
@@ -2,6 +2,6 @@ class OneDayJob < ApplicationJob
   def perform
     Hook.delete_all
     RawHook.delete_all
-    DailyPacket.produce_for(Date.today)
+    DailyPacket.save_to_s3
   end
 end
--- app/models/daily_packet.rb
+++ app/models/daily_packet.rb
@@ -1,5 +1,5 @@
 class DailyPacket
-  def self.produce_for(date)
+  def self.save_to_s3(date = Date.today)
     file_path = "daily-packets/#{date.strftime("%Y-%m-%d")}.pdf"
     packet = DailyPacketView.new(date)
     S3Api.write(file_path, packet.pdf_data)
