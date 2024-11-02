c8ee86f4d95aa53067be051eb17ba1920a3996ab
Jon Allured <jon@jonallured.com>
2024-11-02 13:02:08 -0500

    Support producing daily packets locally

--- lib/tasks/daily_packet.rake
+++ lib/tasks/daily_packet.rake
@@ -1,4 +1,9 @@
 namespace :daily_packet do
+  desc "Produce today's packet and save locally."
+  task save_locally: :environment do
+    DailyPacket.save_locally
+  end
+
   desc "Produce today's packet and save to S3"
   task save_to_s3: :environment do
     DailyPacket.save_to_s3
--- spec/models/daily_packet_spec.rb
+++ spec/models/daily_packet_spec.rb
@@ -1,6 +1,18 @@
 require "rails_helper"
 
 describe DailyPacket do
+  describe ".save_locally" do
+    it "renders and writes the pdf data locally" do
+      mock_view = double(:mock_view)
+      expect(DailyPacketView).to receive(:new).and_return(mock_view)
+
+      expect(mock_view).to receive(:save_as).with("tmp/daily_packet.pdf").and_return(nil)
+
+      date = Date.parse("2001-02-03")
+      DailyPacket.save_locally(date)
+    end
+  end
+
   describe ".save_to_s3" do
     it "renders and writes the pdf data to s3" do
       mock_view = double(:mock_view, pdf_data: "PDF GOES HERE")
