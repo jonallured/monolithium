9fa9bbfde689908a4cdd08233ff10dd11b289628
Jon Allured <jon@jonallured.com>
2024-11-03 10:15:42 -0600

    Move random warm fuzzy into data model

--- app/models/daily_packet.rb
+++ app/models/daily_packet.rb
@@ -1,14 +1,16 @@
 class DailyPacket
   def self.save_locally(date = Date.today)
     file_path = "tmp/daily_packet.pdf"
-    daily_packet = DailyPacket.create(built_on: date)
+    warm_fuzzy = WarmFuzzy.random
+    daily_packet = DailyPacket.create(built_on: date, warm_fuzzy: warm_fuzzy)
     packet = DailyPacketPdfView.new(daily_packet)
     packet.save_as(file_path)
   end
 
   def self.save_to_s3(date = Date.today)
     file_path = "daily-packets/#{date.strftime("%Y-%m-%d")}.pdf"
-    daily_packet = DailyPacket.create(built_on: date)
+    warm_fuzzy = WarmFuzzy.random
+    daily_packet = DailyPacket.create(built_on: date, warm_fuzzy: warm_fuzzy)
     packet = DailyPacketPdfView.new(daily_packet)
     S3Api.write(file_path, packet.pdf_data)
   end
@@ -17,10 +19,11 @@ class DailyPacket
     new(attributes)
   end
 
-  attr_reader :built_on
+  attr_reader :built_on, :warm_fuzzy
 
   def initialize(attributes)
     @built_on = attributes[:built_on]
+    @warm_fuzzy = attributes[:warm_fuzzy]
   end
 
   def built_on_phrase
