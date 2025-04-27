467cd8370ef2f16646310b1115be67ef7fa284d8
Jon Allured <jon@jonallured.com>
2025-04-27 10:00:12 -0600

    Bail when Feedbin data is unexpected

--- app/models/feedbin_stats.rb
+++ app/models/feedbin_stats.rb
@@ -1,9 +1,14 @@
 class FeedbinStats
   def self.compute
     unread_entry_ids = FeedbinApi.get_unread_entries
+    return [] unless unread_entry_ids.any?
+
     first_entry_id = unread_entry_ids.min
     oldest_entry = FeedbinApi.get_entry(first_entry_id)
-    oldest_published_on = Date.parse(oldest_entry["published"])
+    oldest_entry_published = oldest_entry["published"]
+    return [] unless oldest_entry_published
+
+    oldest_published_on = Date.parse(oldest_entry_published)
     oldest_ago = (Date.today - oldest_published_on).to_i
     [unread_entry_ids.count, oldest_ago]
   end
