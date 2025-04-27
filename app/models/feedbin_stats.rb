f7a00d7c91fc1a71af507c01a657c664510ee38a
Jon Allured <jon@jonallured.com>
2025-04-27 10:20:21 -0600

    Use a class to compute Feedbin stats

--- app/models/feedbin_stats.rb
+++ app/models/feedbin_stats.rb
@@ -1,15 +1,40 @@
 class FeedbinStats
   def self.compute
-    unread_entry_ids = FeedbinApi.get_unread_entries
-    return [0, 0] unless unread_entry_ids.any?
+    new.compute
+  end
 
-    first_entry_id = unread_entry_ids.min
-    oldest_entry = FeedbinApi.get_entry(first_entry_id)
-    oldest_entry_published = oldest_entry["published"]
-    return [unread_entry_ids.count, 0] unless oldest_entry_published
+  def initialize
+    @unread_entry_ids = []
+    @oldest_entry = {}
+  end
+
+  def compute
+    get_unread_entries
+    get_oldest_entry
+
+    [unread_count, oldest_ago]
+  end
+
+  def get_unread_entries
+    @unread_entry_ids = FeedbinApi.get_unread_entries
+  end
+
+  def get_oldest_entry
+    return unless @unread_entry_ids.any?
+
+    first_entry_id = @unread_entry_ids.min
+    @oldest_entry = FeedbinApi.get_entry(first_entry_id)
+  end
+
+  def unread_count
+    @unread_entry_ids.count
+  end
+
+  def oldest_ago
+    oldest_entry_published = @oldest_entry["published"]
+    return 0 unless oldest_entry_published
 
     oldest_published_on = Date.parse(oldest_entry_published)
-    oldest_ago = (Date.today - oldest_published_on).to_i
-    [unread_entry_ids.count, oldest_ago]
+    (Date.today - oldest_published_on).to_i
   end
 end
