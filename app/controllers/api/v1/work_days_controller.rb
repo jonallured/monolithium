0ff0902f45910c2e4835284915840c0885a32845
Jon Allured <jon@jonallured.com>
2025-07-19 10:05:10 -0500

    Add view WorkDays API endpoint

---
+++ app/controllers/api/v1/work_days_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V1
+    class WorkDaysController < Api::V1Controller
+      before_action :ensure_work_week_params, only: :index
+
+      expose(:work_days) do
+        work_week = WorkWeek.find_or_create(params[:year], params[:number])
+        work_week.work_days
+      end
+
+      private
+
+      def ensure_work_week_params
+        error = nil
+        error = "year is required" if params[:year].nil?
+        error = "number is required" if params[:number].nil?
+        return if error.nil?
+
+        render json: {error: error}, status: :bad_request
+      end
+    end
+  end
+end
