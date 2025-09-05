f2c3fc1328e88553fc674cdc238b32e1d57ce36f
Jon Allured <jon@jonallured.com>
2025-09-05 16:39:45 -0500

    Remove Sidekiq config

--- config/initializers/sidekiq.rb
+++
@@ -1,6 +0,0 @@
-if Rails.env.development?
-  redis_db = ENV.fetch("REDIS_DB", 0)
-  redis_config = {url: "redis://localhost:6379/#{redis_db}"}
-  Sidekiq.configure_server { |config| config.redis = redis_config }
-  Sidekiq.configure_client { |config| config.redis = redis_config }
-end
