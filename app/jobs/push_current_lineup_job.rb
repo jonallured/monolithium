f87e102b3b7278de94633e0208362da27fb6424c
Jon Allured <jon@jonallured.com>
2025-09-05 15:43:23 -0500

    Extract associated object for sending lineups

--- app/jobs/push_current_lineup_job.rb
+++ app/jobs/push_current_lineup_job.rb
@@ -1,6 +1,5 @@
 class PushCurrentLineupJob < ApplicationJob
   def perform
-    artworks = Lineup.current.as_of(Time.zone.now)
-    ArtsyViewerChannel.broadcast(artworks)
+    Lineup::Sender.push_current
   end
 end
