6493d8f49655aec5b4847671d8b57a8c06822990
Jon Allured <jon@jonallured.com>
2025-12-28 13:05:26 -0600

    Admin views detail reports

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,4 +1,20 @@
 module AnalyticsHelper
+  def detail_description(analytics_report)
+    item_count = analytics_report.matching_items.count.to_fs(:delimited)
+    metric = analytics_report.metric_name
+    current_page = analytics_report.paginated_items.current_page
+    total_pages = analytics_report.paginated_items.total_pages
+    page_info = "(page #{current_page} of #{total_pages})"
+
+    [
+      "Detail of",
+      pluralize(item_count, "matching item"),
+      "with",
+      metric,
+      page_info
+    ].join(" ") + "."
+  end
+
   def summary_description(analytics_report)
     item_count = analytics_report.matching_items.count.to_fs(:delimited)
     grouping = analytics_report.metric_name
--- app/models/analytics/summary_report.rb
+++ app/models/analytics/summary_report.rb
@@ -1,28 +1,5 @@
 module Analytics
-  class SummaryReport
-    METRIC_TO_FIELD_MAP = {
-      "browser" => :browser_name,
-      "page" => :request_path,
-      "referrer" => :referrer_host
-    }
-
-    def self.for(options)
-      report = new(options)
-      report.calculate
-      report
-    end
-
-    attr_reader :matching_items, :metric_name, :period_start, :rows
-
-    def initialize(options)
-      @metric_name = options[:metric].singularize
-
-      @period_start = Date.new(
-        options[:year].to_i,
-        options[:month].to_i
-      )
-    end
-
+  class SummaryReport < BaseReport
     def calculate
       @matching_items = ApacheLogItem.where(requested_at: @period_start.all_month)
       report_field = METRIC_TO_FIELD_MAP[@metric_name]
@@ -31,21 +8,8 @@ module Analytics
       @rows = summary_rows.sort
     end
 
-    def prev_params
-      prev_start = @period_start - 1.month
-      params_for(prev_start)
-    end
-
-    def next_params
-      next_start = @period_start + 1.month
-      params_for(next_start)
-    end
-
-    private
-
-    def params_for(start)
-      month, year = start.strftime("%m %Y").split
-      {month: month, year: year}
+    def to_partial_path
+      "summary"
     end
   end
 end
