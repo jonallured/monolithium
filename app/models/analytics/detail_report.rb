3b02c65067af5d5b31f38a23fec6557a536d74cf
Jon Allured <jon@jonallured.com>
2025-12-31 09:46:21 -0600

    Link to CRUD show page from detail reports

--- app/models/analytics/detail_report.rb
+++ app/models/analytics/detail_report.rb
@@ -11,8 +11,10 @@ module Analytics
       @matching_items = ApacheLogItem.where(requested_at: @period_start.all_month)
       @paginated_items = matching_items.order(requested_at: :asc).page(@page)
       report_field = METRIC_TO_FIELD_MAP[@metric_name]
-      plucked_values = paginated_items.pluck(report_field, :requested_at)
-      detail_rows = plucked_values.map { |label, requested_at| DetailRow.new(label, requested_at) }
+      plucked_values = paginated_items.pluck(:id, report_field, :requested_at)
+      detail_rows = plucked_values.map do |id, label, requested_at|
+        DetailRow.new(id, label, requested_at)
+      end
       @rows = detail_rows
     end
 
