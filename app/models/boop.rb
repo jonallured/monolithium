2e45e33065e4ebffe20dc79d4791fc836924d29d
Jon Allured <jon@jonallured.com>
2026-02-12 13:17:53 -0600

    Add Boops page

--- app/models/boop.rb
+++ app/models/boop.rb
@@ -1,10 +1,24 @@
 class Boop < ApplicationRecord
+  def self.latency
+    next_boop = Boop.next
+    return 0 unless next_boop
+    Time.now.utc - next_boop.created_at
+  end
+
   def self.next
-    where(dismissed_at: nil).order(created_at: :asc).first
+    pending.first
   end
 
   def self.next_number
     (Boop.maximum(:number) || 0) + 1
+  end
+
+  def self.oldest
+    where.not(dismissed_at: nil).order(created_at: :asc).first
+  end
+
+  def self.pending
+    where(dismissed_at: nil).order(created_at: :asc)
   end
 
   def dismiss!
