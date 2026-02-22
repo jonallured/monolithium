cb0ac0a8b2a0e5877cd5ac3fda96e0ff1bb90c9c
Jon Allured <jon@jonallured.com>
2026-02-22 17:26:40 -0600

    Fix flaky ping request spec by freezing time

--- spec/requests/api/v1/ping_spec.rb
+++ spec/requests/api/v1/ping_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 describe "GET /api/v1/ping" do
   it "returns the current time" do
-    get "/api/v1/ping"
+    freeze_time do
+      get "/api/v1/ping"
 
-    expect(response.status).to eq 200
-    expect(response.body).to eq Time.now.to_i.to_s
+      expect(response.status).to eq 200
+      expect(response.body).to eq Time.now.to_i.to_s
+    end
   end
 end
